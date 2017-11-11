8f1c41386b3e25929cabfd1dbdb21ca4b377c4dd
Karan Motani <karan.motani94@gmail.com>
2017-11-11 14:29:51 -0600

    remove word from signature and context

--- lesk.py
+++ lesk.py
@@ -1,8 +1,9 @@
 import sys
 import nltk
-# nltk.download('wordnet')
+nltk.download('wordnet')
 from nltk.corpus import wordnet
 
+# Read the Inputs: the input sentence and the word
 def readInput():
     # input = sys.argv[1]
     input = 'The bank can guarantee deposits will eventually cover future tuition costs ' \
@@ -13,39 +14,49 @@ def readInput():
     return input, word
 
 
+# Simplied Lesk Algorithm Function
 def lesk(input, word):
 
-    bestSense = 'n'
-    maxOverlap = 0
     context = input.split()
-    print('Context: ', context)
+    context.remove(word)
 
     synsets = wordnet.synsets(word)
+    maxOverlap = 0
+    bestSense = 0
+
+    # Initial best-sense <- most frequent sense for word
+    if synsets[0].name().__contains__('.' + 'n' + '.'):
+        bestSense = synsets[0].definition()
+    print('Input sentence: ', input)
+    print('Word: ', word)
+    print('Best Sense: ', bestSense)
+    print('\n')
 
     for synset in synsets:
         if synset.name().__contains__('.' + 'n' + '.'):
             print('Name: ', synset.name())
             print('Definition: ', synset.definition())
             print('Example: ', synset.examples())
-            # print('Lemma name: ', synset.lemmas()[0].name())
 
             # Signature
             signature = str(synset.definition()) + ' '
             for example in synset.examples():
                 signature += str(example)
-            signature = signature.replace('(', '').replace(')', '')
+            signature = signature.replace('(', '').replace(')', '').replace(';', '').replace(',', '').replace('.', '')
             signature = signature.split()
-            print('Signature: ', signature, '\n')
+            if word in signature:
+                signature.remove(word)
 
             overlap = len(set(context) & set(signature))
-
+            print('Overlap: ', overlap)
             if overlap > maxOverlap:
                 maxOverlap = overlap
                 bestSense = synset.definition()
-            #
-            # print('Sense: ', bestSense)
-            # print('Overlap: ', maxOverlap)
 
+                print('Best Sense changed: ', bestSense)
+            print('\n')
+
+    print('\n----------------------------------------------- OUTPUT -----------------------------------------------\n')
     print('Best Sense: ', bestSense)
     print('Max Overlap: ', maxOverlap)
 
